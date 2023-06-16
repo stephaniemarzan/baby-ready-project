396a04e7618d53b6e00ce23769a7522959916905
Stephanie Marzan <s.marzanoberholz@ufl.edu>
2023-06-15 21:31:20 -0500

    set up api call to bitly

--- shopping-list.php
+++ shopping-list.php
@@ -18,10 +18,32 @@
     <main>
         <h1>Shopping List</h1>
 
-        <form action="">
+        <form action="shopping-list.php" method="post">
             <input type="text" name="link-entry" id="link-entry" placeholder="Link entry here.">
-            <button>Shorten Link</button>
+            <button type="submit">Shorten Link</button>
         </form>
+
+
+        <?php
+
+        $api_url = "https://api-ssl.bitly.com/v4/bitlinks";
+        $token = "5e4e23c45796f9c9464a228731c4fdea5eadee1a";
+        $url = $_POST ['link-entry'];
+    
+        $ch = curl_init($api_url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(["long_url" => $url]));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Authorization: Bearer $token",
+        "Content-Type: application/json"
+        ]);
+  
+        $link_result = json_decode(curl_exec($ch));
+
+        ?>
+
+        <p>Your shortened URL is: <?php echo $link_result->link;  ?></p> 
+
     </main>
 
 </body>

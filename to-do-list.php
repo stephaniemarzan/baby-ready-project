cb66b8ba637574a804e94646e0ab5852c417dbdb
Stephanie Marzan <s.marzanoberholz@ufl.edu>
2023-06-16 15:55:58 -0500

    delete task function in to do list

--- to-do-list.php
+++ to-do-list.php
@@ -6,22 +6,36 @@ $mysqli = require __DIR__ . "/database.php";
 
 $userid = $_SESSION["user_id"];
 
+$error = "";
+
+$task = $_POST['task'];
+
     if(isset($_POST['submit'])){
 
-        $task = $_POST['task'];
+        if(empty($task)){
+            $error = "Please type your task to add it.";
+        }else{
+            $sql = "INSERT INTO tasks (task, userid) VALUES ('$task','$userid')";
      
-        $sql = "INSERT INTO tasks (task, userid) VALUES ('$task','$userid')";
+            $stmt = $mysqli -> stmt_init();
+            
+            if (! $stmt->prepare($sql)){
+             die("Error: " . $mysqli->error);
+             }
+         
+             if ($stmt->execute()){
+                 header("Location: to-do-list.php");
+                 exit;
+             } 
+        }
      
-        $stmt = $mysqli -> stmt_init();
-        
-        if (! $stmt->prepare($sql)){
-         die("Error: " . $mysqli->error);
-         }
-     
-         if ($stmt->execute()){
-             header("Location: to-do-list.php");
-             exit;
-         } 
+}
+
+if(isset($_GET['delete_task'])){
+    $taskid = $_GET['delete_task'];
+    mysqli_query($mysqli,"DELETE FROM tasks WHERE id=$taskid");
+    header('location: to-do-list.php');
+
 }
 
     $tasks = mysqli_query($mysqli, "SELECT * FROM tasks WHERE userid = '$userid'");
@@ -50,6 +64,13 @@ $userid = $_SESSION["user_id"];
         <h1>To-Do List</h1>
 
         <form action="to-do-list.php" method="post">
+        
+        <?php if(isset($error)){ ?>
+
+            <p><?php echo $error ?></p>
+
+            <?php } ?>
+
             <input type="text" name="task" id="taskentry" placeholder="Type task here.">
             <button type="submit" name="submit">Add Task</button>
         </form>
@@ -71,7 +92,7 @@ $userid = $_SESSION["user_id"];
                     <td><?php echo $tasknumber++; ?></td>
                     <td><?php echo $row['task']; ?></td>
                     <td>
-                        <a href="#">x</a>
+                        <a href="to-do-list.php?delete_task=<?php echo $row['id'];?>">x</a>
                     </td>
                 </tr>
             <?php
